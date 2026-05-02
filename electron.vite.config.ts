4972b5eeafb61f342b45693b7ba77f2205b8c940
flamboh <oliver.boorstein@gmail.com>
2026-05-02 13:22:22 -0700

    fix lock

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,6 +1,7 @@
-import { resolve } from 'path'
+import { resolve } from 'node:path'
 import { defineConfig } from 'electron-vite'
 import react from '@vitejs/plugin-react'
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
@@ -12,7 +13,14 @@ export default defineConfig({
         '@renderer': resolve('src/renderer/src')
       }
     },
-    plugins: [react(),
+    plugins: [
+      tanstackRouter({
+        target: 'react',
+        routesDirectory: './src/renderer/src/routes',
+        generatedRouteTree: './src/renderer/src/routeTree.gen.ts',
+        autoCodeSplitting: true
+      }),
+      react(),
       tailwindcss()
     ]
   }
